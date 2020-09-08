aa1317621d3710a0eba0bc4c2b28f3939a623b1a
Clemens Boos <clemensboos@gmail.com>
2020-09-08 18:09:32 +0200

    Move module to file

--- flake.nix
+++ flake.nix
@@ -16,61 +16,6 @@
 
     defaultPackage.x86_64-linux = pkgs.prometheus-fritzscraper-exporter;
 
-    nixosModules.prometheus-fritzscraper-exporter = { config, pkgs, lib, ... }: with lib; let
-      cfg = config.services.prometheus-fritzscraper-exporter;
-    in {
-      options.services.prometheus-fritzscraper-exporter =  {
-        enable = mkEnableOption "prometheus-fritzscraper-exporter";
-
-        listenPort = mkOption {
-          type = types.int;
-          default = 8140;
-          description = "Port that the FRITZ!Scraper Prometheus exporter listens on";
-        };
-
-        gatewayAddress = mkOption {
-          type = types.str;
-          default = "fritz.box";
-          description = ''
-            The hostname or IP of the FRITZ!Box.
-          '';
-        };
-
-        username = mkOption {
-          type = types.str;
-          default = "root";
-          description = ''
-            The username to used authenticate with the FRITZ!Box.
-          '';
-        };
-
-        password = mkOption {
-          type = types.str;
-          default = "";
-          description = ''
-            The password to used authenticate with the FRITZ!Box.
-          '';
-        };
-      };
-
-      config = mkIf cfg.enable {
-        # nixpkgs.overlays = [ self.overlay ];
-
-        systemd.services."prometheus-fritzscraper-exporter" = let
-          myPkgs = pkgs.appendOverlays [ self.overlay ];
-        in {
-          wantedBy = [ "multi-user.target" ];
-          after = [ "network.target" ];
-          serviceConfig.Restart = mkDefault "always";
-          serviceConfig.PrivateTmp = mkDefault true;
-          serviceConfig.WorkingDirectory = mkDefault "/tmp";
-
-          serviceConfig.ExecStart = ''
-            ${myPkgs.prometheus-fritzscraper-exporter}/bin/prometheus-fritzscraper-exporter \
-              ${toString cfg.listenPort} ${cfg.gatewayAddress} ${cfg.username} ${cfg.password}
-          '';
-        };
-      };
-    };
+    nixosModules.prometheus-fritzscraper-exporter = import ./module.nix self;
   };
 }
---
+++ module.nix
@@ -0,0 +1,58 @@
+flake: { config, pkgs, lib, ... }:
+
+with lib;
+
+let
+  cfg = config.services.prometheus-fritzscraper-exporter;
+in {
+  options.services.prometheus-fritzscraper-exporter =  {
+    enable = mkEnableOption "prometheus-fritzscraper-exporter";
+
+    listenPort = mkOption {
+      type = types.int;
+      default = 8140;
+      description = "Port that the FRITZ!Scraper Prometheus exporter listens on";
+    };
+
+    gatewayAddress = mkOption {
+      type = types.str;
+      default = "fritz.box";
+      description = ''
+        The hostname or IP of the FRITZ!Box.
+      '';
+    };
+
+    username = mkOption {
+      type = types.str;
+      default = "root";
+      description = ''
+        The username to used authenticate with the FRITZ!Box.
+      '';
+    };
+
+    password = mkOption {
+      type = types.str;
+      default = "";
+      description = ''
+        The password to used authenticate with the FRITZ!Box.
+      '';
+    };
+  };
+
+  config = mkIf cfg.enable {
+    systemd.services."prometheus-fritzscraper-exporter" = let
+      myPkgs = pkgs.appendOverlays [ flake.overlay ];
+    in {
+      wantedBy = [ "multi-user.target" ];
+      after = [ "network.target" ];
+      serviceConfig.Restart = mkDefault "always";
+      serviceConfig.PrivateTmp = mkDefault true;
+      serviceConfig.WorkingDirectory = mkDefault "/tmp";
+
+      serviceConfig.ExecStart = ''
+        ${myPkgs.prometheus-fritzscraper-exporter}/bin/prometheus-fritzscraper-exporter \
+          ${toString cfg.listenPort} ${cfg.gatewayAddress} ${cfg.username} ${cfg.password}
+      '';
+    };
+  };
+}
